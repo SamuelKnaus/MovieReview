a5e422a1c83080bda818cb0dc13cf887801a4a2d
Jonah Siedler <jonah.siedler@gmail.com>
2022-03-29 13:05:24 +0300

    add an example component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Test
+          message="sdd"
+        />
         <p>
           Edit
           {' '}
